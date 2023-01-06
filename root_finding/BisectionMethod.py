66c6d5cb05e4ce8a0fdee9ee78d4ce351bcdb71f
rasteiro11 <titico0306@gmail.com>
2023-01-06 16:19:18 -0300

    feat-root-finding: added root finding algorithms

---
+++ root_finding/BisectionMethod.py
@@ -0,0 +1,19 @@
+def f(x):
+    return x*x-2
+
+def bisection_method(x_neg, x_pos, eps=1e-8):
+    x = 0
+    
+    while abs(x_pos - x_neg) > eps:
+        x = (x_pos + x_neg) / 2
+
+        if f(x) > 0:
+            x_pos = x
+        else:
+            x_neg = x 
+
+    return x 
+
+if __name__ == "__main__":
+    print(bisection_method(-2, 2))
+
